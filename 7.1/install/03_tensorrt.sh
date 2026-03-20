6c43d9baff211cd417546fb21160ba8179c4994c
dinesh-maharana087 <dkmaharana087@gmail.com>
2026-03-20 18:50:50 +0530

    Fix line endings permanently (LF)

--- 7.1/install/03_tensorrt.sh
+++ 7.1/install/03_tensorrt.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/../config/versions.env"
 source "$SCRIPT_DIR/../utils/logger.sh"
-source "$SCRIPT_DIR/../utils/check.sh"
+source "$SCRIPT_DIR/../utils/checks.sh"
 
 echo "🔍 Checking TensorRT..."
 
@@ -19,14 +19,23 @@ echo "Installing TensorRT..."
 
 version=$TENSORRT_VERSION
 
+sudo apt-get update
+
 sudo apt-get install -y \
 libnvinfer-dev=${version} \
 libnvinfer-dispatch-dev=${version} \
 libnvinfer-dispatch10=${version} \
 libnvinfer-headers-dev=${version} \
+libnvinfer-headers-plugin-dev=${version} \
+libnvinfer-lean-dev=${version} \
+libnvinfer-lean10=${version} \
 libnvinfer-plugin-dev=${version} \
+libnvinfer-plugin10=${version} \
+libnvinfer-vc-plugin-dev=${version} \
+libnvinfer-vc-plugin10=${version} \
 libnvinfer10=${version} \
 libnvonnxparsers-dev=${version} \
+libnvonnxparsers10=${version} \
 tensorrt-dev=${version}
 
 echo "✅ TensorRT installation completed"
